fa6aadb855259c799cdff67b66240280606442e2
rchaser53 <tayoshizawa29@gmail.com>
2018-12-04 20:27:33 +0900

    create creator

---
+++ src/creator.rs
@@ -0,0 +1,38 @@
+use std::path::Path;
+
+use inkwell::builder::Builder;
+use inkwell::context::Context;
+use inkwell::module::Module;
+use inkwell::targets::{InitializationConfig, Target};
+
+pub struct LLVMCreator {
+    pub context: Context,
+    pub builder: Builder,
+    pub module: Module,
+}
+
+impl LLVMCreator {
+    pub fn new(module_name: &str) -> LLVMCreator {
+        Target::initialize_native(&InitializationConfig::default()).unwrap();
+
+        let context = Context::create();
+        let module = context.create_module(module_name);
+        let builder = context.create_builder();
+
+        LLVMCreator {
+            builder: builder,
+            module: module,
+            context: context,
+        }
+    }
+
+    #[allow(dead_code)]
+    pub fn dump(&self) {
+        &self.module.print_to_stderr();
+    }
+
+    #[allow(dead_code)]
+    pub fn emit_file(&self, filename: &str) {
+        let _ = self.module.print_to_file(Path::new(filename));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,58 +1,56 @@
 extern crate inkwell;
 
 use std::path::Path;
-
-use inkwell::context::Context;
-use inkwell::targets::{InitializationConfig, Target};
 use std::error::Error;
 
-fn run() -> Result<(), Box<Error>> {
-    let context = Context::create();
-    let module = context.create_module("main");
-    let builder = context.create_builder();
+mod creator;
+use self::creator::*;
 
-    let i64_type = context.i64_type();
-    let bool_type = context.bool_type();
+fn run() -> Result<(), Box<Error>> {
+    let lc = LLVMCreator::new("main");
+
+    let i64_type = lc.context.i64_type();
+    let bool_type = lc.context.bool_type();
     let fn_type = i64_type.fn_type(&[i64_type.into(), i64_type.into(), i64_type.into()], false);
 
-    let parent = module.add_function("main", fn_type, None);
-    let basic_block = context.append_basic_block(&parent, "entry");
-    builder.position_at_end(&basic_block);
+    let parent = lc.module.add_function("main", fn_type, None);
+    let basic_block = lc.context.append_basic_block(&parent, "entry");
+    lc.builder.position_at_end(&basic_block);
 
     let true_value = bool_type.const_int(1, false);
 
     // build branch
-    let then_bb = context.append_basic_block(&parent, "then");
-    let else_bb = context.append_basic_block(&parent, "else");
-    let cont_bb = context.append_basic_block(&parent, "ifcont");
+    let then_bb = lc.context.append_basic_block(&parent, "then");
+    let else_bb = lc.context.append_basic_block(&parent, "else");
+    let cont_bb = lc.context.append_basic_block(&parent, "ifcont");
 
-    builder.build_conditional_branch(true_value, &then_bb, &else_bb);
+    lc.builder
+        .build_conditional_branch(true_value, &then_bb, &else_bb);
 
-    builder.position_at_end(&then_bb);
+    lc.builder.position_at_end(&then_bb);
     let then_val = i64_type.const_int(1, false);
-    builder.build_unconditional_branch(&cont_bb);
+    lc.builder.build_unconditional_branch(&cont_bb);
 
-    let then_bb = builder.get_insert_block().unwrap();
-    builder.position_at_end(&else_bb);
+    let then_bb = lc.builder.get_insert_block().unwrap();
+    lc.builder.position_at_end(&else_bb);
     let else_val = i64_type.const_int(2, false);
-    builder.build_unconditional_branch(&cont_bb);
+    lc.builder.build_unconditional_branch(&cont_bb);
 
-    builder.position_at_end(&cont_bb);
+    lc.builder.position_at_end(&cont_bb);
 
-    let phi = builder.build_phi(context.i64_type(), "iftmp");
+    let phi = lc.builder.build_phi(lc.context.i64_type(), "iftmp");
 
     phi.add_incoming(&[(&then_val, &then_bb), (&else_val, &else_bb)]);
 
     let ret_val = phi.as_basic_value().into_int_value();
 
-    builder.build_return(Some(&ret_val));
+    lc.builder.build_return(Some(&ret_val));
 
-    module.print_to_file(Path::new("nyan.ll"))?;
+    lc.module.print_to_file(Path::new("nyan.ll"))?;
 
     Ok(())
 }
 
 fn main() {
-    Target::initialize_native(&InitializationConfig::default()).unwrap();
     run().unwrap();
 }
